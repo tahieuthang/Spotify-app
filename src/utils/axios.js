028dbd94687b94f1754d734400bee4759cd29a96
Kamadee <tahieuthang@gmail.com>
2025-07-28 17:06:30 +0700

    update url api

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://spotify-api-c05y.onrender.com',
   timeout: 5000, // tuỳ chọn
   headers: {
     'Content-Type': 'application/json'
