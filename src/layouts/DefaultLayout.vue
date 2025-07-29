a1abaf206c4199f07c38e9f30021186751ae69fc
Kamadee <tahieuthang@gmail.com>
2025-07-29 09:52:58 +0700

    update file name

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col gap-4 h-screen w-full bg-black p-3 overflow-hidden">
-    <TopNavbar />
+    <TopNavBar />
     <div class="h-screen flex gap-3 overflow-hidden">
       <SideBar class="w-1/4 overflow-y-auto" />
       <div class="w-3/4 flex flex-col overflow-y-auto">
@@ -13,7 +13,7 @@
 
 <script setup>
 import { ref } from 'vue'
-import TopNavbar from '@/components/dashboard/TopNavbar.vue'
+import TopNavBar from '@/components/dashboard/TopNavBar.vue'
 import Footer from '@/components/dashboard/Footer.vue'
 import SideBar from '@/components/dashboard/SideBar.vue'
 import MusicPlayerBar from '@/components/dashboard/MusicPlayerBar.vue'
